d785a137f8b0d33a6f967c051d1256e3c0a498bc
Camilo Lopez <calopez.monroy@gmail.com>
2024-11-07 08:37:21 -0500

    feat(web): POC con navbar y diferentes rutas

--- shell-app/src/app/app-routing.module.ts
+++ shell-app/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { 
@@ -11,7 +14,11 @@ const routes: Routes = [
       return microfrontend1().MicroFrontend1Module;
     },
   },
-  { path: '**', redirectTo: '' }
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/microfrontend1', pathMatch: 'full' },
+  { path: '**', redirectTo: '/microfrontend1' }
 ];
 
 @NgModule({
--- shell-app/src/app/app.module.ts
+++ shell-app/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
